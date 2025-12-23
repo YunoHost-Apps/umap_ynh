dd92f7d523b1a128a89479167e14ce153cc72fc4
DeMiro5001 <amir@abdelhedi.com.tn>
2025-12-23 01:49:17 +0100

    fix

--- conf/setup_user.py
+++
@@ -1,9 +0,0 @@
-def setup_project_user(user):
-    """
-    All users used the Django admin, so we need to set the "staff" user flag.
-    Called from django_yunohost_integration.sso_auth
-    """
-    # user.is_staff = True
-    # user.is_superuser = True
-    # user.save()
-    return user
