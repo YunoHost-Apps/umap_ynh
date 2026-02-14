d98d0c9dd93cd34bea16c38d7fa000e33a88169f
Florent <florent.git@zeteo.me>
2026-02-14 11:38:59 +0100

    Various cleanups

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -6,7 +6,6 @@
 
 # Transfer the main SSO domain to the App:
 ynh_current_host=$(cat /etc/yunohost/current_host)
-__YNH_CURRENT_HOST__=${ynh_current_host} # FIXME: Useful? Sounds like not, and there is a confusion.
 
 umap_with_extra_deps="umap-project[yunohost,sync]"
 
@@ -17,28 +16,9 @@ umap_with_extra_deps="umap-project[yunohost,sync]"
 # e.g.: point pip cache to: /home/yunohost.app/$app/.cache/
 XDG_CACHE_HOME="$data_dir/.cache/"
 
-log_path=/var/log/$app
-log_file="${log_path}/${app}.log"
-
 #=================================================
 # HELPERS
 #=================================================
-
-#==================================================================================
-# Until we get a newer Python in YunoHost, see:
-# https://forum.yunohost.org/t/use-newer-python-than-3-9/22568
-#==================================================================================
-
-#==================================================================================
-#==================================================================================
-
-myynh_setup_log_file() {
-    mkdir -p "$(dirname "$log_file")"
-    touch "$log_file"
-
-    chown -c -R $app:$app "$log_path"
-    chmod -c u+rwx,o-rwx "$log_path"
-}
 
 myynh_fix_file_permissions() {
     # /var/www/$app/
