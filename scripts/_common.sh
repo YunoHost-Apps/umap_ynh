41e151f2971054a9f3bd582acf0f96ae5a2cc2da
Yohan Boniface <yohanboniface@free.fr>
2026-02-25 22:43:28 +0100

    Change source logic to try to fix CI

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-source /usr/share/yunohost/helpers
-
 #=================================================
 # RETRIEVE ARGUMENTS FROM THE MANIFEST
 #=================================================
